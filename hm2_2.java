3d6c60bf3bcdbfc4fe13b5f49affd5b948041d89
twang666 <60125624+twang666@users.noreply.github.com>
2020-01-21 21:39:21 +0900

    Add files via upload

---
+++ hm2_2.java
@@ -0,0 +1,13 @@
+
+public class hm2_2 {
+public static void main(String[] args) {
+	int a = 2;
+	switch (a) {
+	case 2: System.out.println("two");break;
+	case 1: System.out.println("one");break;
+	default: System.out.println("???");break;
+	}
+	
+	
+}
+}
---
+++ hw2.java
@@ -0,0 +1,19 @@
+
+public class hw2 {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+		if (a == 2) {
+			System.out.println("two");
+
+		} else if (a == 1) {
+			System.out.println("one");
+		} else {
+			System.out.println("???");
+		}
+		
+	
+	}
+
+}
